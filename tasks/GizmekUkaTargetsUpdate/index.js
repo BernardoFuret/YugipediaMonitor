71e73303d812bad512195d275ccacfd16b8b2b14
BernardoFuret <bernardo.furet@hotmail.com>
2024-06-12 12:33:36 +0100

    Update Gizmek Uka targets script:
    Fix finding if it can be Special Summoned from the Deck.
    Add if it's Normal Monster.
    Remove dependency on hardcoded stats.

--- tasks/GizmekUkaTargetsUpdate/index.js
+++ tasks/GizmekUkaTargetsUpdate/index.js
@@ -1,26 +1,22 @@
 /**
- * AdsTxtUpdate
+ * Gizmek Uka targets update
  */
 
 const { promises: fsp } = require( 'fs' );
 
-const fetch = require( "node-fetch" );
+const fetch = require( 'node-fetch' );
 
-const STATS = require( './stats.js' );
+const DiscordLogger = require( '../../DiscordLogger.js' );
 
-const DiscordLogger = require( "../../DiscordLogger.js" );
+const ScheduledTask = require( '../ScheduledTask.js' );
 
-const ScheduledTask = require( "../ScheduledTask.js" );
-
-const props = require( "./properties.json" );
+const props = require( './properties.json' );
 
 const { dumpFile } = require( './config.json' );
 
-function makeQuery( stat ) {
-	const query = `[[Medium::TCG||OCG]][[ATK string::${stat}]][[DEF string::${stat}]][[Belongs to::Main Deck]][[Summoning::Can be Special Summoned]]|?Attribute|?Type|?Stars string|?Summoning|?Primary type|limit=500`;
+const smwQuery = `[[Medium::TCG||OCG]][[Misc::Equal ATK and DEF]][[Belongs to::Main Deck]][[Summoning::Can be Special Summoned]]|?ATK|?DEF|?Attribute|?Type|?Stars string|?Summoning|?Primary type|limit=1000`;
 
-	return `https://yugipedia.com/api.php?action=ask&query=${encodeURIComponent( query )}&format=json`;
-}
+const smwQueryApiurl = `https://yugipedia.com/api.php?action=ask&query=${encodeURIComponent( smwQuery )}&format=json`;
 
 async function updateHtmlPage( monsters ) {
 	const content = await fsp.readFile( dumpFile, 'utf8' );
@@ -36,28 +32,27 @@ async function updateHtmlPage( monsters ) {
 async function fetchData() {
 	const monsters = [];
 
-	for ( const stat of STATS ) {
-		const response = await fetch( makeQuery( stat ) ).then( r => r.json() );
+	const response = await fetch( smwQueryApiurl ).then( r => r.json() );
 
-		Object.entries( response.query.results ).forEach( ( [ monster, { printouts } ] ) => {
-			if (
-				monster.match( '(original)' )
-				||
-				printouts[ 'Primary type' ].some( t => t.fulltext.match( /ritual/i ) )
-			) {
-				return;
-			}
+	Object.entries( response.query.results ).forEach( ( [ monster, { printouts } ] ) => {
+		if (
+			monster.match( '(original)' )
+			||
+			printouts[ 'Primary type' ].some( t => t.fulltext.match( /ritual/i ) )
+		) {
+			return;
+		}
 
-			monsters.push( {
-				name: monster,
-				atkDef: stat,
-				attribute: ( printouts.Attribute[ 0 ] || {} ).fulltext,
-				type: ( printouts.Type[ 0 ] || {} ).fulltext,
-				level: printouts[ 'Stars string' ][ 0 ],
-				ssFromDeck: !printouts.Summoning.includes( 'Cannot be Special Summoned from the Deck' ),
-			} );
+		monsters.push( {
+			name: monster,
+			atkDef: printouts.ATK[ 0 ] || printouts.DEF[ 0 ],
+			attribute: ( printouts.Attribute[ 0 ] || {} ).fulltext,
+			type: ( printouts.Type[ 0 ] || {} ).fulltext,
+			level: printouts[ 'Stars string' ][ 0 ],
+			ssFromDeck: !printouts.Summoning.find( o => o.fulltext === 'Cannot be Special Summoned from the Deck' ),
+			isNormalMonster: !!printouts[ 'Primary type' ].find(o => o.fulltext.match( /Normal/ ) )
 		} );
-	}
+	} );
 
 	return monsters;
 }
